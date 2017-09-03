2f85ce088a8f48a09efea1e7369e01ffbd33fe1c
Muzaffer barış <muzafferbaris4@gmail.com>
2017-09-04 01:20:48 +0300

    Update ÖzYenilemeliFonksiyon.py

--- ÖzYenilemeliFonksiyon.py
+++ ÖzYenilemeliFonksiyon.py
@@ -1,10 +1,15 @@
 # python
-
+#değerlerin bulunduğu bir sözlük oluşturuyoruz
 sözlük = {"Edirne":"22","Malatya":"44","İzmir":"35","Balıkesir":"10"}
+#kullanıcıdan plakasını öğrenmek istediği ilin adını alıyoruz 
 sorgula = input("Plaka kodunu öğrenmek istediğiniz ilin ismini giriniz: ")
+#while ile sonsuz bir döngü oluşturuyoruz
 while True:
+    #eğer kullanıcıdan gelen veri q'ya eşitse break ile döngüyü kırıp programdan çıkış yapıyoruz
     if sorgula == "q":
         break
+    #eğer kullanıcıdan gelen veri q değilse 
     elif sorgula:
+        #sorgula değişkenini sözlük içindeki değerlerle karşılaştırıp eşleşen veriyi ekrana yazdırıyoruz 
         print(sözlük.get(sorgula,"Böyle Bir İl Bulunamadı!"))
         sorgula = input("Plaka kodunu öğrenmek istediğiniz ilin ismini giriniz: ")
